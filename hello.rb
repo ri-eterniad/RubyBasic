7ace70faa0ca9597a649a403f72358db4e08bd03
YoshihiroIshizka <mdmdmdmdmd57@gmail.com>
2021-05-06 11:33:00 +0900

    %s%d%fなど書式付き文字列に値を埋め込む学習

--- hello.rb
+++ hello.rb
@@ -1,33 +1,64 @@
-puts "hello"
-puts %Q(hello)
-puts %(hello)
+p "name: %s" % "kiryu"
+p "name: %8s" % "kiryu"
+p "name: %-8s" % "kiryu"
 
-puts 'hello'
-puts %(hello)
+p "id: %d" % 4
+p "id: %4d" % 4
+p "id: %-4d" % 4
 
-puts "he\"llo"
-puts %Q(he"llo)
-puts %(he"llo)
+p "score: %10.5f" % 4.6567
+p "score: %3.8f" % 4.6567
+p "score: %-10.5f" % 4.6567
 
-puts 'he\'llo'
-puts %q(he'llo)
 
-puts %W(base"ball,socc"er)
-puts %w(base'ball,socc'er)
+
+#値が複数の場合
+p "id: %3d, rate: %4.2f" % [12,3.548]
+
+#printf sprintf
+printf("name:%10s\n","kiryu")
+
+puts sprintf("name:%10s\n","kiryu")
+
+puts sprintf("nameId:%05d, rate: %5.2f",478 , 5.9848)
+
+
 
 =begin
+# "文字列" % 値
+# %s
+# 実行結果
+
+# %d
+# %f
+
 実行結果
-hello
-hello
-hello
-hello
-hello
-he"llo
-he"llo
-he"llo
-he'llo
-he'llo
-base"ball,socc"er
-base'ball,socc'er
+"name: kiryu"
+"name:    kiryu"
+"name: kiryu   "
+"id: 4"
+"id:    4"
+"id: 4   "
+"score:    4.65670"
+"score: 4.65670000"
+"score: 4.65670   "
+"id:  12, rate: 3.55"
+name:     kiryu
+name:     kiryu
+nameId:00478, rate: $5.2f
+yoshi@YoshihiakanoMBP src % ruby hello.rb
+"name: kiryu"
+"name:    kiryu"
+"name: kiryu   "
+"id: 4"
+"id:    4"
+"id: 4   "
+"score:    4.65670"
+"score: 4.65670000"
+"score: 4.65670   "
+"id:  12, rate: 3.55"
+name:     kiryu
+name:     kiryu
+nameId:00478, rate:  5.98
 
 =end
