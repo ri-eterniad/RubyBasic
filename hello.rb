cdf4533070b52d364decccddcb263a658fa5ade8
YoshihiroIshizka <mdmdmdmdmd57@gmail.com>
2021-05-07 22:53:08 +0900

    メソッドについて

--- hello.rb
+++ hello.rb
@@ -1,63 +1,22 @@
+module Movie
 
-class Foods
-
-    attr_accessor:name
-
-    @@count =0
-
-    VER =1.1
-
-    def initialize(name)
-        @@count += 1
-        @name =name
+    def self.encode
+        puts "encoding.."
     end
-
-    def description
-        puts "これは#{self.name}"
-        foodPrivate
-    end
-
-    private
-        def foodPrivate
-            puts "ptivateメソッドです"
-        end
-    
-
-    def self.info
-        puts "VER#{VER}これはクラスメソッド　現在#{@@count}個のインスタンスが作られました"
-    end
-
-    
-
-end
-
-class Fruits < Foods
-
-    def deli
-        puts "#{@name}美味しいな"
-    end
-
-    #オーバライド 元のFoodsクラスのdescriptionを
-    def description
-        puts "これは美味しい#{@name}です"
-        foodPrivate
-    end
-
-    def foodPrivate
-        puts "privateメソッドを上書き"
+    def self.export
+        puts "exporting.."
     end
 end
 
-Foods.new("food").description
+Movie.encode
+Movie.export
 
-grape = Fruits.new("grape")
-grape.deli
-grape.description
+
 =begin
 実行
+モジュール
+インスタンスの作成や継承できない
 
-イニシャライズメソッドとクラスの外に書いたメソッドは
-自動的にprivateになる
--レシーバーを指定できない
-
+このように関連するメソッドや定数などをざっとまとめて
+グループ化したいだけのとき便利
 =end
